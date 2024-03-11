cf24d952dfdb62b9a3680bb80b444c8bc069f563
Rivaldy Fauzan <rifalous@gmail.com>
2024-03-11 15:32:01 +0700

    create basic blog using ruby on rails

---
+++ test/models/blog_post_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class BlogPostTest < ActiveSupport::TestCase
+  test "draft? returns true for draft blog post" do
+    binding.irb
+    BlogPost.new(published_at: nil).draft?
+  end
+end
